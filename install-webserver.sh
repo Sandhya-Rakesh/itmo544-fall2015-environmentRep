4fd6efbd2cbae798fb0e97566a714b9c034e003d
Sandhya Gupta <sandhyaguptamp@gmail.com>
2015-10-25 21:38:22 -0500

    Added php5-mysql package

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 php5-curl curl mysql-client php-mysqli
+sudo apt-get install -y apache2 git php5 php5-curl curl mysql-client php5-mysql
 
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
@@ -10,7 +10,7 @@ git clone https://github.com/SandhyaGupta/itmo544-fall2015-applicationRep.git
 
 mv ./itmo544-fall2015-applicationRep/images /var/www/html/images
 mv ./itmo544-fall2015-applicationRep/index.html /var/www/html
-mv ./itmo544-fall2015-applicationRep/images/*.php /var/www/html
+mv ./itmo544-fall2015-applicationRep/*.php /var/www/html
 sudo mv vendor /var/www/html &> /tmp/movevendor.txt
 
 sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
