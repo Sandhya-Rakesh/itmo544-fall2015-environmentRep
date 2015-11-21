f4d1cb97762ddbbdfd07a0526c8e1cecac12f6ad
Sandhya Gupta <sandhyaguptamp@gmail.com>
2015-11-21 13:19:11 -0600

    Reverted back to original script

--- install-webserver.sh
+++ install-webserver.sh
@@ -6,12 +6,12 @@ sudo apt-get install -y apache2 git php5 php5-curl curl mysql-client php5-mysql
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
-git clone https://github.com/SandhyaGupta/itmo544-fall2015-applicationRep-2.git
+git clone https://github.com/SandhyaGupta/itmo544-fall2015-applicationRep.git
 
-mv ./itmo544-fall2015-applicationRep/content /var/www/html/content
-mv ./itmo544-fall2015-applicationRep/lib /var/www/html/lib
-#mv ./itmo544-fall2015-applicationRep/js /var/www/html/js
-mv ./itmo544-fall2015-applicationRep/*.html /var/www/html
+mv ./itmo544-fall2015-applicationRep/images /var/www/html/images
+mv ./itmo544-fall2015-applicationRep/css /var/www/html/css
+mv ./itmo544-fall2015-applicationRep/js /var/www/html/js
+mv ./itmo544-fall2015-applicationRep/index.html /var/www/html
 mv ./itmo544-fall2015-applicationRep/*.php /var/www/html
 sudo mv vendor /var/www/html &> /tmp/movevendor.txt
 
